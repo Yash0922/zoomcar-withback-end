41622e13f11845c6b193bd3e34e2ae0e3ac0bde7
yash0922 <yashkumarpal987@gmail.com>
2023-04-20 21:59:44 +0530

    update

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -30,7 +30,7 @@ export default function ProductDetails() {
   }
 
   useEffect(() => {
-    getData(` https://relieved-cyan-monkey.cyclic.app/${id}`);
+    getData(` https://relieved-cyan-monkey.cyclic.app/cars/${id}`);
   }, []);
 
   return (
